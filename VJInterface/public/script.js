951f78698ad68c9df21d99d4d6d34bf1260ec3b9
bevchou <31548243+bevchou@users.noreply.github.com>
2018-11-25 18:02:03 -0500

    updated w/ cleaned up code

--- VJInterface/public/script.js
+++ VJInterface/public/script.js
@@ -21,62 +21,72 @@ let shapes = {
 //GET DOM ELEMENTS
 window.addEventListener('load', function() {
   //get gif dom elements
-  gifMode = document.getElementById("gifMode");
-  getGifWord = document.getElementById("getGifWord");
+  let gifMode = document.getElementById("gifMode");
+  let getGifWord = document.getElementById("getGifWord");
   //get abstract visual dom elements
-  avMode = document.getElementById("avMode");
+  let avMode = document.getElementById("avMode");
   //foreground colors
-  fgHue = document.getElementById("fgHue");
-  fgSat = document.getElementById("fgSat");
-  fgLig = document.getElementById('fgLig');
+  let fgHue = document.getElementById("fgHue");
+  let fgSat = document.getElementById("fgSat");
+  let fgLig = document.getElementById('fgLig');
   //background colors
-  bgHue = document.getElementById("bgHue");
-  bgSat = document.getElementById("bgSat");
-  bgLig = document.getElementById("bgLig");
+  let bgHue = document.getElementById("bgHue");
+  let bgSat = document.getElementById("bgSat");
+  let bgLig = document.getElementById("bgLig");
   //shapes
-  square = document.getElementById("square");
-  circle = document.getElementById("circle");
-  triangle = document.getElementById("triangle");
-  squiggle = document.getElementById("squiggle");
-  arc = document.getElementById("arc");
+  let square = document.getElementById("square");
+  let circle = document.getElementById("circle");
+  let triangle = document.getElementById("triangle");
+  let squiggle = document.getElementById("squiggle");
+  let arc = document.getElementById("arc");
 
 
-
-
+  //set gif mode
   gifMode.addEventListener('click', function() {
-    //can't have both gif and abstract mode on at the same time
+    //can't have both modes on at the same time
     if (gifMode.checked && avMode.checked) {
       gifMode.checked = true;
       avMode.checked = false;
     }
     if (gifMode.checked) {
+      //gif mode on
       mode = 1;
     } else {
+      //both modes off
       mode = 0;
     }
     console.log("POST", mode);
   });
 
+  //set abstract visual mode
   avMode.addEventListener('click', function() {
+    //can't have both modes on at the same time
     if (gifMode.checked && avMode.checked) {
       gifMode.checked = false;
       avMode.checked = true;
     }
     if (avMode.checked) {
+      //visual mode on
       mode = 2;
     } else {
+      //both modes off
       mode = 0;
     }
     console.log("POST", mode);
   });
 
+  //get the gif keyword
   getGifWord.addEventListener('keyup', function(e) {
     e.preventDefault();
+    //post when user hits enter
+    //field cannot be empty
     if (e.keyCode === 13 && this.value != "") {
       console.log("POST", this.value);
     }
   });
 
+  //for all slider inputs
+  //post updated color objects
   function postSlider(elemName, key, obj) {
     elemName.addEventListener('mouseup', function() {
       obj[key] = this.value;
@@ -92,19 +102,19 @@ window.addEventListener('load', function() {
   postSlider(bgSat, "sat", bgColor);
   postSlider(bgLig, "lig", bgColor);
 
-
-  function postShape(shapeName, key, obj) {
+  //for all checkboxes
+  //post updated shape object
+  function postCheckbox(shapeName, key, obj) {
     shapeName.addEventListener("click", function() {
       obj[key] = this.checked;
       console.log("POST", obj);
     });
   }
 
-  postShape(square, "square", shapes);
-  postShape(circle, "circle", shapes);
-  postShape(triangle, "triangle", shapes);
-  postShape(squiggle, "squiggle", shapes);
-  postShape(arc, "arc", shapes);
-
+  postCheckbox(square, "square", shapes);
+  postCheckbox(circle, "circle", shapes);
+  postCheckbox(triangle, "triangle", shapes);
+  postCheckbox(squiggle, "squiggle", shapes);
+  postCheckbox(arc, "arc", shapes);
 
 });
